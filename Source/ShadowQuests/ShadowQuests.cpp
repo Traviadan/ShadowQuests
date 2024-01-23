c252af74b60b95cb984312de22bad092641add8e
Thorsten Eggers <thorsten@eggers-bhv.de>
2024-01-23 17:41:42 +0100

    Projekt erstellt mit Common UI Plugin

---
+++ Source/ShadowQuests/ShadowQuests.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShadowQuests.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShadowQuests, "ShadowQuests" );
