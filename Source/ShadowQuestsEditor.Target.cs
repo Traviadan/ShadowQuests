55c3a0964ed37e8ba18a0ecc4e744c6a7d7fc74a
Thorsten Eggers <thorsten@eggers-bhv.de>
2024-01-23 18:13:26 +0100

    Target.cs edited

--- Source/ShadowQuestsEditor.Target.cs
+++ Source/ShadowQuestsEditor.Target.cs
@@ -9,7 +9,7 @@ public class ShadowQuestsEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ShadowQuests");
 	}
 }
