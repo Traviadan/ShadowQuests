6f47b23666db1f28466ef0e71a94a1384d15dfbc
Thorsten Eggers <thorsten@eggers-bhv.de>
2024-01-23 18:21:07 +0100

    Update ShadowQuests.Build.cs

--- Source/ShadowQuests/ShadowQuests.Build.cs
+++ Source/ShadowQuests/ShadowQuests.Build.cs
@@ -8,9 +8,20 @@ public class ShadowQuests : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"Niagara",
+            "EnhancedInput"
+		});
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+		PrivateDependencyModuleNames.AddRange(new string[] {
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
