7ad7a93e52f9d6ac28d8ca87436991664659543b
Thorsten Eggers <thorsten@eggers-bhv.de>
2024-01-23 22:09:51 +0100

    SQGameModeBase created

---
+++ Source/ShadowQuests/Public/SQGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 Thorsten Eggers <thorsten@eggers-bhv.de>
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SQGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWQUESTS_API ASQGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
