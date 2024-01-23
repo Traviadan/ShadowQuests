f96ab352e83d5ab7b23d1a85c6b4949d2dce495b
Thorsten Eggers <thorsten@eggers-bhv.de>
2024-01-23 19:32:28 +0100

    SQAssetManager Class created

---
+++ Source/ShadowQuests/Public/SQAssetManager.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 Thorsten Eggers <thorsten@eggers-bhv.de>
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "SQAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWQUESTS_API USQAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
