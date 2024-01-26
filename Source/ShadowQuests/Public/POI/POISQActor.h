8f19f739d394ee9b30e3aa3df163872e19792b82
Thorsten Eggers <thorsten@eggers-bhv.de>
2024-01-26 09:21:20 +0100

    POISQActor added

---
+++ Source/ShadowQuests/Public/POI/POISQActor.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 Thorsten Eggers <thorsten@eggers-bhv.de>
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SQActor.h"
+#include "POISQActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWQUESTS_API APOISQActor : public ASQActor
+{
+	GENERATED_BODY()
+	
+};
